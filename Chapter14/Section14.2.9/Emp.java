869fd1c9155a81106fad53be43b04386dfe42304
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2019-09-18 17:18:08 +0900

    added

--- Chapter14/Section14.2.9/Emp.java
+++ Chapter14/Section14.2.9/Emp.java
@@ -1,6 +1,17 @@
 record Emp(String name, int age) {
-    @Override
-	public String toString() {
-	    return "Emp:" + name + " " + age;
+    // overloading constructor
+    public Emp(String name, String style) {
+        this.name = name;
+        if (style.equals("COOL")) age = 20;
+        else if (style.equals("SAFE")) age = 30;
+        else if (style.equals("ELEGANT")) age = 50;
+        else age = 70;
+    }
+    public String fancyOutput() { // additional method
+        return "My style is COOL";
+    }
+
+	public void setAge(int age) {
+			this.age = age;
 	}
 }
