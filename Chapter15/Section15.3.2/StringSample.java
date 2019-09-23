4ac0a4b1b7a7b461fa0575fb21a380c0928cca51
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2019-09-24 06:48:27 +0900

    Added

--- Chapter15/Section15.3.2/StringSample.java
+++ Chapter15/Section15.3.2/StringSample.java
@@ -1,6 +1,6 @@
 class StringSample {
     public static void main(String[] args) {
         String html = "<HTML>\n\t<BODY>\n\t\t<H1>Meaning in life</H1>\n\t</BODY>\n</HTML>";
-		System.out.printf(html);
+		System.out.println(html);
     }
 }
---
+++ Chapter15/Section15.3.3/StringSample.java
@@ -0,0 +1,17 @@
+class StringSample {
+    public static void main(String[] args) {
+        String html = "<HTML>" +
+"\n\t" + "<BODY>" +
+"\n\t\t" + "<H1>Meaning of life</H1>" +
+"\n\t" + "</BODY>" +
+"\n" + "</HTML>";
+		System.out.println(html);
+
+		String html2 = "<HTML>" +
+                   "\n\t" + "<BODY>" +
+                       "\n\t\t" + "<H1>Meaning of life</H1>" +
+                   "\n\t" + "</BODY>" +
+              "\n" + "</HTML>";
+		System.out.println(html2);
+    }
+}
