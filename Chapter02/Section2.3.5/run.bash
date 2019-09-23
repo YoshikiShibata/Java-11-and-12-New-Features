8000ec618a3d34853536cc7be23363a7dfdbc98d
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2019-09-23 14:44:11 +0900

    Added and updated

--- Chapter02/Section2.3.5/run.bash
+++ Chapter02/Section2.3.5/run.bash
@@ -2,7 +2,11 @@
 
 # Copyright (C) 2019 Yoshiki Shibata. All rights reserved.
 
-rm -f appcds.jar AppCDS.class com/ejavaguru/appcds/*.class myappCDS.lst
+rm -f appcds.jar \
+		AppCDS.class \
+		com/ejavaguru/appcds/*.class \
+		myappCDS.lst \
+		appCDS.jsa
 
 javac AppCDS.java com/ejavaguru/appcds/*.java
 if [ $? != 0 ]; then
@@ -23,3 +27,11 @@ echo ""
 echo "Search myappCDS.lst file:"
 grep -n AppCDS myappCDS.lst
 grep -n com/ejavaguru/appcds/ myappCDS.lst
+
+java -Xshare:dump -XX:SharedClassListFile=myappCDS.lst -XX:SharedArchiveFile=appCDS.jsa -cp appcds.jar
+if [ $? != 0 ]; then
+	exit 1
+fi
+
+java -Xshare:on -XX:SharedArchiveFile=appCDS.jsa -cp appcds.jar AppCDS
+
