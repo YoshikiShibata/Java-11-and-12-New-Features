e410cec30b31319d2ff0027db04f7cdd48ad49ed
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2019-09-24 06:44:05 +0900

    Added

---
+++ Chapter15/Section15.3.2/StringSample.java
@@ -0,0 +1,6 @@
+class StringSample {
+    public static void main(String[] args) {
+        String html = "<HTML>\n\t<BODY>\n\t\t<H1>Meaning in life</H1>\n\t</BODY>\n</HTML>";
+		System.out.printf(html);
+    }
+}
