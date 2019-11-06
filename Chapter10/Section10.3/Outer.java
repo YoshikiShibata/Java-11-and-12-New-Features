3156532230cdd03c270a80300ae9184d93961e20
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2019-11-07 07:05:55 +0900

    updated

--- Chapter10/Section10.3/Outer.java
+++ Chapter10/Section10.3/Outer.java
@@ -5,4 +5,11 @@ public class Outer {
         // InnerはouterIntへアクセスできるか
         int innerInt = outerInt; 
     }
+
+	public static void main(String[] args) {
+	    // Java 11 or later	
+        for (Class<?> c: Outer.class.getNestMembers()) {
+	        System.out.println(c);
+        }
+	}
 }
