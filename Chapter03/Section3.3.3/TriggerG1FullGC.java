57b1be227595e9b55996af9e315371364fcbd1ff
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2019-12-11 06:47:37 +0900

    updated

--- Chapter03/Section3.3.3/TriggerG1FullGC.java
+++ Chapter03/Section3.3.3/TriggerG1FullGC.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
+
 public class TriggerG1FullGC {
     final BufferedImage[] images = new BufferedImage[999999];
     final byte[][] imgByte = new byte[999999][];
@@ -19,6 +20,7 @@ public class TriggerG1FullGC {
         }
         System.out.println("Bytes read");
     }
+
     public static void main(String... args) throws Exception {
         new TriggerG1FullGC();
     }
