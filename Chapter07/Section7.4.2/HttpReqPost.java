7b355cb9659a01d1344da6144319851027681212
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2020-02-04 06:36:15 +0900

    updated

--- Chapter07/Section7.4.2/HttpReqPost.java
+++ Chapter07/Section7.4.2/HttpReqPost.java
@@ -5,7 +5,7 @@ import java.net.http.HttpResponse;
 
 public class HttpReqPost {
     public static void main(String uri[]) throws Exception {
-        String postData = "?name='Mala'&email='info@ejavaguru@gmail.com'";
+        String postData = "?name=Mala&email=info@ejavaguru@gmail.com";
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://www.ejavaguru.com/Java11/register.php")).POST(HttpRequest.BodyPublishers.ofString(postData)).build();
         HttpResponse<?> response = client.send(request, HttpResponse.BodyHandlers.discarding());
